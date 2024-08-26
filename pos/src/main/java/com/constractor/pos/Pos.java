a0d54ea1618eb621fb51e231deb9f5476b20875e
Mohammed A. Lahim <askerson87@gmail.com>
2024-08-26 11:35:11 +0500

    first coomit for point of Sale

---
+++ pos/src/main/java/com/constractor/pos/Pos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.constractor.pos;
+
+/**
+ *
+ * @author M.ASKAR
+ */
+public class Pos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
